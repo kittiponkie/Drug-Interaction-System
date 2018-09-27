cc73e7b7980ae6032ab736e672a138f9063e3f23
kittipon <kittipon.kei@gmail.com>
2018-09-27 21:23:09 +0700

    select content

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,7 +5,8 @@ import Posts from '@/components/Posts'
 import NewPost from '@/components/NewPost'
 import EditPost from '@/components/EditPost'
 import interaction from '@/components/interaction'
-
+import DrugInformation from '@/components/DrugInformation'
+import Drug_Interaction from '@/components/Drug_Interaction'
 Vue.use(Router)
 
 export default new Router({
@@ -40,6 +41,33 @@ export default new Router({
       path: '/interaction',
       name: 'interaction',
       component: interaction
+    },
+    {
+      path: '/DrugInformation',
+      name: 'DrugInformation',
+      component: DrugInformation
+    },
+    {
+      path: '/Drug_Interaction',
+      name: 'Drug_Interaction',
+      component: Drug_Interaction
     }
   ]
 })
+
+//import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+//import 'vue-material/dist/theme/black-green-dark.css'
+import 'vue-material/dist/theme/black-green-light.css'
+//import 'vue-material/dist/theme/default-dark.css'
+//import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
+
+//import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
