36ca7fc3b01b0042250c786f3c8c82ce953c48c0
winpluem <win.washira@gmail.com>
2018-10-23 03:19:54 +0700

    Return Sum Json

--- server2/src/app.js
+++ server2/src/app.js
@@ -15,79 +15,69 @@ app.listen(8082, () => {
   console.log('Server is up and listening on 8082 port ...')
 })
 
+function isEmptyObject(obj) {
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 app.get('/info/GP/:GenName', (req, res) => {
   console.log('Fetching user with id :' + req.params.GenName)
   const GenName = req.params.GenName+"%"
+  var CheckGenName 
   console.log(GenName)
-  var sql = "SELECT GPID , FSN FROM gp WHERE FSN LIKE ?;SELECT GPUID , FSN FROM gpu WHERE FSN LIKE ?;";
-  connection.query(sql, [GenName,GenName], (err, rows, fields) => {
+  var sql = "SELECT GPID , FSN FROM gp WHERE FSN LIKE ?;SELECT GPUID , FSN FROM gpu WHERE FSN LIKE ?;SELECT * FROM tp WHERE TPID IN ( SELECT TPID FROM gptotp WHERE GPID IN ( SELECT GPID FROM gp WHERE FSN LIKE ? )); SELECT * FROM tpu WHERE TPUID IN ( SELECT TPUID FROM tptotpu WHERE TPID IN ( SELECT TPID FROM gptotp WHERE GPID IN ( SELECT GPID FROM gp WHERE FSN LIKE ? )));";
+  connection.query(sql, [GenName,GenName,GenName,GenName], (err, rows, fields) => {
     if (err) {
       console.log('Failed to query for users : ' + err)
       res.sendStatus(500)
       return
     }
-    //console.log('Successfully')
     
-    const info1 = rows[0].map((row) => {
+    if (isEmptyObject(rows[0])){
+      CheckGenName = 0
+    }
+    else CheckGenName = 1 
+
+    const info1 = rows[0].map((row) => { 
       return {
         GPID : row.GPID ,
-        FSN_GPID : row.FSN ,
+        FSN_GPID : row.FSN
       }
     })
-
     const info2 = rows[1].map((row) => {
       return {
         GPUID : row.GPUID ,
         FSN_GPUID : row.FSN
       }
     })
-    
+    const info3 = rows[2].map((row) => {
+      return {
+        TPID : row.TPID ,
+        FSN_TPID : row.FSN ,
+        MANUFACTURER : row.MANUFACTURER ,
+        CHANGEDATE : row.CHANGEDATE
+      }
+    })
+    const info4 = rows[3].map((row) => {
+      return {
+        TPUID : row.TPUID ,
+        FSN_TPUID : row.FSN ,
+        MANUFACTURER : row.MANUFACTURER ,
+        CHANGEDATE : row.CHANGEDATE
+      }
+    })
     var result =  {
+      CheckGenName: CheckGenName ,
       GP: info1,
-      GPU: info2
+      GPU: info2,
+      TP: info3,
+      TPU: info4
     };
 
     res.json(result)
-  })
-})
-
-app.get('/info/TP/:GenName', (req, res) => {
-  console.log('Fetching user with id :' + req.params.GenName)
-  const GenName = req.params.GenName+"%"
-  console.log(GenName)
-  const query = 'SELECT TPID , FSN FROM tp WHERE FSN LIKE ? ;'
-  connection.query(query, [GenName], (err, rows, fields) => {
-    if (err) {
-      console.log('Failed to query for users : ' + err)
-      res.sendStatus(500)
-      return
-    }
-    //console.log('Successfully')
-    const info = rows.map((row) => {
-      return {
-        GPID : row.TPID ,
-        FSN : row.FSN}
-    })
-    res.json(info)
-  })
-})
-app.get('/info/TPU/:GenName', (req, res) => {
-  console.log('Fetching user with id :' + req.params.GenName)
-  const GenName = req.params.GenName+"%"
-  console.log(GenName)
-  const query = 'SELECT TPUID , FSN FROM tpu WHERE FSN LIKE ? ;'
-  connection.query(query, [GenName], (err, rows, fields) => {
-    if (err) {
-      console.log('Failed to query for users : ' + err)
-      res.sendStatus(500)
-      return
-    }
-    //console.log('Successfully')
-    const info = rows.map((row) => {
-      return {
-        GPID : row.TPUID ,
-        FSN : row.FSN}
-    })
-    res.json(info)
   })
 })
