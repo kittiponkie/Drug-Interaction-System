21316364bb760caec08c091b632c2d74e6ab824b
Wachira <win.washira@gmail.com>
2019-03-18 20:12:27 +0700

    Allergic drug Feature

--- server/src/app.js
+++ server/src/app.js
@@ -753,27 +753,12 @@ app.get("/AllergicDrug/:PatientID", (req, res) => {
 app.post('/post/AllergicDrug', (req, res) => {
   var db = req.db;
   var PatientID = req.body.PatientID
-  var GPName = req.body.GPName
-  var RXCUI = req.body.RXCUI
-  var VTMID = req.body.VTMID
+  var VTMName = req.body.VTMName
 
   console.log("POST Method")
   var new_AllergicDrug = new AllergicDrug({
     PatientID: PatientID,
-    GPName: GPName,
-    VTMID: VTMIpp.post('/post/AllergicDrug', (req, res) => {
-  var db = req.db;
-  var PatientID = req.body.PatientID
-  var GPName = req.body.GPName
-  var RXCUI = req.body.RXCUI
-  var VTMID = req.body.VTMID
-
-  console.log("POST Method")
-  var new_AllergicDrug = new AllergicDrug({
-    PatientID: PatientID,
-    GPName: GPName,
-    VTMID: VTMID,
-    RXCUI: RXCUI
+    VTMName: VTMName
   })
   new_AllergicDrug.save(function (error) {
     if (error) {
@@ -786,15 +771,15 @@ app.post('/post/AllergicDrug', (req, res) => {
   })
 })
 
-// Delete AllergicDrug By PatientID and GPName 
+// Delete AllergicDrug By PatientID and VTMName 
 app.delete('/remove/AllergicDrug/:PatientID/:GPName', (req, res) => {
   var db = req.db;
   var PatientID = req.params.PatientID
-  var GPName = req.params.GPName
+  var VTMName = req.params.VTMName
 
   AllergicDrug.remove({
     "PatientID": PatientID,
-    "GPName": GPName
+    "VTMName": VTMName
   }, function (err, post) {
     if (err)
       res.send(err)
