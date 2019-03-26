6aea121ad7956ecffa332699a1240f71d7a9c2f2
Wachira <win.washira@gmail.com>
2019-03-26 13:13:47 +0700

    .

--- server/models/UnconfirmOrder.js
+++ server/models/UnconfirmOrder.js
@@ -1,7 +1,30 @@
 const mongoose = require("mongoose");
 const UnconfirmOrderSchema = mongoose.Schema({
     PatientID: String,
-    VTMName: String
+    DoctorID: String,
+    Duration: {
+        year: String,
+        month: String,
+        day: String
+    },
+    GPName: String,
+    RXCUI: String,
+    Dosage: {
+        dose: String,
+        unit: String
+    },
+    Frequency: {
+        mor: String,
+        aft: String,
+        eve: String,
+        bed: String,
+        before: String,
+        after: String,
+        symptoms: String
+    },
+    Times: String,
+    Quantity: String,
+    Description: String
 })
 
 module.exports = mongoose.model('UnconfirmOrder', UnconfirmOrderSchema)
